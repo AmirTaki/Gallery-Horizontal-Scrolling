8b1ae44b1e774b58ab957ed1c85ba7926450f91a
amir <amirtaki623@gmail.com>
2025-05-20 22:13:38 +0330

    js: scroll wheel

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 let scrollContainer = document.querySelector(".gallery")
 let backBtn = document.getElementById('backBtn')
 let nextBtn = document.getElementById('nextBtn')
+
+
+scrollContainer.addEventListener("wheel", (event)=>{
+    event.preventDefault();
+    scrollContainer.scrollLeft += event.deltaY
+    console.log(screenLeft)
+    console.log(event.deltaY)
+})
